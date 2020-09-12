65d9102f6597452a6912027e30e8fa2e009974f1
xcmyz <xcmyz@outlook.com>
2020-09-12 22:33:15 +0800

    Test gate target

--- train.py
+++ train.py
@@ -107,8 +107,8 @@ def main(args):
                     [i for i in range(mel_length.size(0))]).long().to(device)
                 index_arr = torch.cat(
                     [index_arr.unsqueeze(1), (mel_length-1).unsqueeze(1)], 1)
-                print(index_arr)
-                # gate_target[index_arr] = 1.
+                # print(index_arr)
+                gate_target[index_arr] = 1.
                 print(gate_target)
 
                 # Forward
